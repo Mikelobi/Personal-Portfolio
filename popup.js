a22ff9fa79753c52bff4eba30336e5a1ab17885c
Michael Ugochukwu <mark.omulu@gmail.com>
2022-03-09 10:08:22 +0000

    Add more javascript modal functions

--- popup.js
+++ popup.js
@@ -3,18 +3,27 @@ const modal = document.getElementById('myModal');
 const modal2 = document.getElementById('myModal2');
 const modal3 = document.getElementById('myModal3');
 const modal4 = document.getElementById('myModal4');
+const modal5 = document.getElementById('myModal5');
+const modal6 = document.getElementById('myModal6');
+const modal7 = document.getElementById('myModal7');
 
 // Get the button that opens the modal
 const btn = document.getElementById('myBtn');
 const btn2 = document.getElementById('myBtn2');
 const btn3 = document.getElementById('myBtn3');
 const btn4 = document.getElementById('myBtn4');
+const btn5 = document.getElementById('myBtn5');
+const btn6 = document.getElementById('myBtn6');
+const btn7 = document.getElementById('myBtn7');
 
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName('close-work')[0];
 const span2 = document.getElementsByClassName('close-work2')[0];
 const span3 = document.getElementsByClassName('close-work3')[0];
 const span4 = document.getElementsByClassName('close-work4')[0];
+const span5 = document.getElementsByClassName('close-work5')[0];
+const span6 = document.getElementsByClassName('close-work6')[0];
+const span7 = document.getElementsByClassName('close-work7')[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -24,10 +33,19 @@ btn2.onclick = function () {
   modal2.style.display = 'block';
 };
 btn3.onclick = function () {
-  modal2.style.display = 'block';
+  modal3.style.display = 'block';
 };
 btn4.onclick = function () {
-  modal2.style.display = 'block';
+  modal4.style.display = 'block';
+};
+btn5.onclick = function () {
+  modal5.style.display = 'block';
+};
+btn6.onclick = function () {
+  modal6.style.display = 'block';
+};
+btn7.onclick = function () {
+  modal7.style.display = 'block';
 };
 
 // When the user clicks on <span> (x), close the modal
@@ -43,24 +61,19 @@ span3.onclick = function () {
 span4.onclick = function () {
   modal4.style.display = 'none';
 };
+span5.onclick = function () {
+  modal5.style.display = 'none';
+};
+span6.onclick = function () {
+  modal6.style.display = 'none';
+};
+span7.onclick = function () {
+  modal7.style.display = 'none';
+};
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target === modal) {
     modal.style.display = 'none';
-  }
-};
-window.onclick = function (event) {
-  if (event.target === modal2) {
-    modal2.style.display = 'none';
-  }
-};
-window.onclick = function (event) {
-  if (event.target === modal3) {
-    modal3.style.display = 'none';
-  }
-};
-window.onclick = function (event) {
-  if (event.target === modal4) {
-    modal4.style.display = 'none';
   }
 };
